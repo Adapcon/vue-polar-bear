917fed2e8c3cebf93a4024923a454b430957e94a
Jeferson Alves <jeferson.a.oficial@gmail.com>
2021-10-08 14:51:20 -0300

    fix: change import folder in main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import '@pb/index.scss'
 import '@pb/normalize.less'
 
 import './plugins/fontawesome';
-import CustomDirectives from './plugin/directives';
+import CustomDirectives from './plugins/directives';
 export * from './components'
 
 Vue.use(CustomDirectives);
