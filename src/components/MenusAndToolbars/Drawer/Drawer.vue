c2c269a1ca8efbe43b7dba2fdcdc08e6f8763da2
Gabriel <gabrielhk3@gmail.com>
2022-01-19 17:13:14 -0300

    Update src/components/MenusAndToolbars/Drawer/Drawer.vue
    Co-authored-by: Jeferson Alves <jeferson.a.oficial@gmail.com>

--- src/components/MenusAndToolbars/Drawer/Drawer.vue
+++ src/components/MenusAndToolbars/Drawer/Drawer.vue
@@ -38,9 +38,11 @@ import PbIcon from '@pb/Miscellaneous/Icon/Icon';
 
 export default {
   name: 'PbDrawer',
+  
   components: {
     PbIcon,
   },
+  
   props: {
     /**
      * Defines the maximum width of the drawer for different device sizes.
@@ -58,12 +60,14 @@ export default {
       default: false,
     },
   },
+  
   emits: [
     /**
      * Event handler for closing the drawer.
      */
     'close',
   ],
+  
   computed: {
     drawerClass() {
       return {
@@ -73,6 +77,7 @@ export default {
       };
     },
   },
+  
   methods: {
     close() {
       this.$emit('close', this.visible);
