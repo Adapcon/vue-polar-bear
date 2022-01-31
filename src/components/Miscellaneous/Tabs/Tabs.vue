aabffc6bcd07861858c8b93ff50887063a81af60
Planivic <xhd184@gmail.com>
2022-01-31 17:20:55 -0300

    feat: creation of new tab

--- src/components/Miscellaneous/Tabs/Tabs.vue
+++ src/components/Miscellaneous/Tabs/Tabs.vue
@@ -4,32 +4,80 @@
     :style="style"
   >
     <template v-for="(label, tab) in tabs">
-      <p
+      <div
         :key="tab"
-        class="pb tab-title"
-        @click="$emit('update:selected-tab', tab)"
+        class="tab-content"
       >
-        <template v-if="selectedTab === tab">
-          <b class="pb" :style="`color: var(--color-${color})`">
-            {{ label }}
-          </b>
-
-          <hr :style="`margin: 10px 0 0 0; border: 1px solid var(--color-${color})`">
-        </template>
-        <template v-else>{{ label }}</template>
-      </p>
+        <p
+          :class="state.editTab ? 'pb tab-title-editable' : 'pb tab-title' "
+          @click="$emit('update:selected-tab', tab)"
+        >
+          <template v-if="selectedTab === tab">
+            <b
+              v-if="!state.editTab"
+              class="pb"
+              :style="`color: var(--color-${color})`"
+            >
+              {{ label }}
+            </b>
+            <input
+              v-if="state.editTab"
+              class="input"
+              :style="{'backgroundColor': colorOpacity}"
+              :placeholder="label"
+              @blur="event => {addInputValue(event, tab)}"
+              @keyup.enter="event => {addInputValue(event, tab)}"
+            >
+            <hr
+              :style="`margin: 10px 0 0 0; border: 1px solid var(--color-${color})`"
+            >
+          </template>
+          <template v-else>{{ label }}</template>
+        </p>
+        <div>
+          <PbButton
+            v-if="state.editTab"
+            :color="selectedTab === tab ? 'primary' : 'gray-10'"
+            button-style="regular"
+            icon="fas fa-times"
+            @click.native="deleteTab(tab)"
+          />
+        </div>
+      </div>
     </template>
+    <div
+      v-if="editableTab"
+      class="edit-buttons"
+    >
+      <PbButton
+        color="primary"
+        button-style="regular"
+        icon="fas fa-plus"
+        @click.native="addTabs()"
+      />
+      <PbButton
+        color="primary"
+        button-style="regular"
+        icon="fas fa-pen"
+        @click.native="state.editTab = !state.editTab"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import { validateColor } from '@pb/utils/validator';
+import PbButton from '../../Buttons/Button/Button.vue';
 
 export default {
   name: 'PbTabs',
+  components: {
+    PbButton,
+  },
 
   props: {
     tabs: { type: Object, default: () => ({}) },
+    dynamicTabs: { type: Object, default: () => ({}) },
     selectedTab: { type: String, default: '' },
     color: {
       type: String,
@@ -37,14 +85,55 @@ export default {
       validator: validateColor,
     },
     hideBorder: { type: Boolean, default: false },
+    editableTab: { type: Boolean, default: false },
+  },
+
+  data() {
+    return {
+      state: {
+        editTab: false,
+        inputValue: 'Tab',
+      },
+    };
   },
 
   computed: {
     style() {
-      if (this.hideBorder)
-        return;
+      if (this.hideBorder) return;
 
       return 'border-bottom: 1px solid';
+    },
+
+    updateTabs: {
+      get() {
+        return this.tabs;
+      },
+
+      set(newValue) {
+        this.$emit('update:tabs', newValue);
+      },
+    },
+
+    colorOpacity() {
+      const color = window.getComputedStyle(document.documentElement).getPropertyValue('--color-primary');
+      return `${color}30`;
+    },
+  },
+
+  methods: {
+    addTabs(value = 'tab') {
+      const valueKey = value.replace(/-./g, x => x[1].toUpperCase());
+      this.$set(this.updateTabs, valueKey, value);
+      this.$nextTick(() => {
+        this.$emit('update:selected-tab', valueKey);
+      });
+    },
+    deleteTab(tab) {
+      this.$delete(this.updateTabs, tab);
+    },
+    addInputValue(event, tab) {
+      this.deleteTab(tab);
+      this.addTabs(event.target.value);
     },
   },
 };
@@ -57,10 +146,33 @@ export default {
   color: var(--color-gray-20);
   flex-wrap: wrap;
 
+  .tab-content {
+    display: flex;
+  }
+
   .tab-title {
     margin-right: 21px !important;
     user-select: none;
     cursor: pointer;
+  }
+
+  .tab-title-editable {
+    cursor: pointer;
+
+    .input {
+      border: none;
+      width: 80px;
+    }
+
+    .input:focus {
+      outline: none;
+    }
+  }
+
+  .edit-buttons {
+    width: 8px;
+    display: flex;
+    padding-bottom: 10px;
   }
 }
 </style>
