5522600d0633aa2fd79abeeae2de7d267132bbb9
Victor Tavares <87718261+planivic1@users.noreply.github.com>
2022-02-04 11:23:35 -0300

    feat: creation of data table component (#33)
    * feat: creation of data table component
    
    * Apply suggestions from code review
    
    Co-authored-by: Daniel Medeiros <51203691+ixpd@users.noreply.github.com>
    
    * fix: pr changes
    
    * fix: pr changes
    
    * fix: pr changes
    
    Co-authored-by: Daniel Medeiros <51203691+ixpd@users.noreply.github.com>

---
+++ src/components/DataVisualization/DataTable/HeaderList.vue
@@ -0,0 +1,51 @@
+<template>
+  <section
+    v-if="settings.showHeader"
+    class="header-list-container pb-row"
+  >
+    <div
+      v-for="(item, index) in tableSchema"
+      :key="index"
+      class="pb-col-2"
+    >
+      <div style="display: flex;">
+        <small class="pb">
+          {{ item.label }}
+        </small>
+        <PbSortIcon
+          v-if="settings.showSortIcon"
+          style="margin: 0 0 0 8px"
+          color="var(--color-gray-90)"
+        />
+      </div>
+    </div>
+    <small class="pb pb-col">
+      Ações
+    </small>
+  </section>
+</template>
+
+<script>
+import PbSortIcon from '../../Miscellaneous/SortIcon/SortIcon.vue';
+
+export default {
+  name: 'HeaderList',
+  components: {
+    PbSortIcon,
+  },
+
+  props: {
+    tableSchema: { type: Array, default: () => ([]) },
+    settings: { type: Object, default: () => ({}) },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header-list-container {
+  background-color: var(--color-gray);
+  padding: 8px;
+  border-bottom: solid 1px #EEEEEE;
+  border-radius: 5px 5px 0 0;
+}
+</style>
