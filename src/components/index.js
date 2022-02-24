549621df28288e157494c28befb4e8d1b4b7ccf9
Gabriel de Oliveira <gabriel.o03@aluno.ifsc.edu.br>
2022-02-24 14:31:38 -0300

    feat: add drag an drop component

--- src/components/index.js
+++ src/components/index.js
@@ -74,3 +74,4 @@ export { default as PbSearchInput } from './FiltersAndSearch/SearchInput/SearchI
 
 // ? Layouts
 export { default as PbLayoutGrid } from './Layout/LayoutGrid/LayoutGrid.vue';
+export { default as PbDragAndDrop } from './Layout/DragAndDrop/DragAndDrop.vue';
