b8667e264bb5f5c2b149ef6aa5ea164fad38c68f
viserion77 <jeferson.a.oficial@gmail.com>
2022-03-05 02:54:47 -0300

    feat(pb-form): create boolean controller

---
+++ src/components/Miscellaneous/Form/Fields/Boolean.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="boolean-container">
+    <PbToggleSwitch
+      v-model="booleanValue"
+      :disabled="onlyShow || entitySchema.dynamic"
+      :placeholder="entitySchema.placeholder"
+      :validator="entitySchema.validator"
+    />
+  </section>
+</template>
+
+<script>
+import PbToggleSwitch from '../../../Inputs/ToggleSwitch/ToggleSwitch.vue';
+
+export default {
+  name: 'BooleanField',
+
+  components: {
+    PbToggleSwitch,
+  },
+
+  props: {
+    entitySchema: { type: Object, default: () => ({}) },
+    onlyShow: { type: Boolean, default: false },
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+
+  computed: {
+    booleanValue: {
+      get() {
+        return this.value || this.entitySchema.defaultValue;
+      },
+      set(newBooleanValue) {
+        this.$emit('input', newBooleanValue);
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.boolean-container{
+}
+</style>
