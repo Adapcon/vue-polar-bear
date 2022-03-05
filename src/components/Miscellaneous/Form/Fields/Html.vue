f5e30f3152a0b0441c9c9dd0a6bacc24099c10a2
Jeferson Alves <jeferson.a.oficial@gmail.com>
2022-03-05 14:48:18 -0300

    feat(pb-form): create html controller (#80)

---
+++ src/components/Miscellaneous/Form/Fields/Html.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="html-container">
+    <PbTextArea
+      v-model="htmlValue"
+      :rows="5"
+      :disabled="onlyShow || entitySchema.dynamic"
+      :placeholder="entitySchema.placeholder"
+      :validator="entitySchema.validator"
+    />
+  </section>
+</template>
+
+<script>
+import PbTextArea from '../../../Inputs/TextArea/TextArea.vue';
+
+export default {
+  name: 'HtmlField',
+
+  components: {
+    PbTextArea,
+  },
+
+  props: {
+    entitySchema: { type: Object, default: () => ({}) },
+    onlyShow: { type: Boolean, default: false },
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+
+  computed: {
+    htmlValue: {
+      get() {
+        return this.value || this.entitySchema.defaultValue;
+      },
+      set(newHtmlValue) {
+        this.$emit('input', newHtmlValue);
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.html-container{
+}
+</style>
