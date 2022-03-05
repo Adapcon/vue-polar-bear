5860a84691b3af3302139ffeafcad5a0b1b9c596
viserion77 <jeferson.a.oficial@gmail.com>
2022-03-05 03:00:51 -0300

    feat(pb-form): create string controller

---
+++ src/components/Miscellaneous/Form/Fields/String.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="string-container">
+    <PbTextInput
+      v-model="stringValue"
+      :placeholder="entitySchema.placeholder"
+      :disabled="onlyShow || entitySchema.dynamic"
+      :validator="entitySchema.validator"
+    />
+  </section>
+</template>
+
+<script>
+import PbTextInput from '../../../Inputs/TextInput/TextInput.vue';
+
+export default {
+  name: 'StringField',
+
+  components: {
+    PbTextInput,
+  },
+
+  props: {
+    entitySchema: { type: Object, default: () => ({}) },
+    onlyShow: { type: Boolean, default: false },
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+
+  computed: {
+    stringValue: {
+      get() {
+        return this.value || this.entitySchema.defaultValue;
+      },
+      set(newStringValue) {
+        this.$emit('input', newStringValue);
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.string-container{
+}
+</style>
