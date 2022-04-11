6b3503b63efe8909c72c7a534f91a7a4a49aa23e
Medeiros <51203691+ixpd@users.noreply.github.com>
2022-04-11 11:05:13 -0300

    included form drawer

--- src/components/Miscellaneous/FormBuilder/FormBuilder.vue
+++ src/components/Miscellaneous/FormBuilder/FormBuilder.vue
@@ -1,20 +1,91 @@
 <template>
   <section class="form-builder-container">
-    <PbForm
-      v-model="formResponse"
-      :entity-schema="state.entitySchema"
+    <div class="form-builder-header">
+      <div class="form-builder-title">
+        <h3 class="pb">Campos Dinâmicos</h3>
+        <p
+          class="pb"
+          style="color: var(--color-gray-20);"
+        >
+          Crie formulários dinâmicos
+        </p>
+      </div>
+      <div class="form-builder-new-field">
+        <PbButton
+          button-style="regular"
+          icon="fas-fa-plus"
+          @click.native="newItem()"
+        >
+          Novo Campo
+        </PbButton>
+      </div>
+    </div>
+
+    <div class="form-builder-created-fields">
+      <div
+        v-for="(field, index) in formResponse"
+        :key="index"
+        class="form-builder-created-field"
+      >
+        <div class="field-container">
+          <small
+            class="pb"
+            style="color: var(--color-gray-40);"
+          >{{
+            index
+          }}</small>
+
+          <div class="field-buttons">
+            <PbButton
+              style="height: 10px;"
+              button-style="icon"
+              icon="fas-fa-trash fa-xs"
+              @click.native="openConfirmModal(index)"
+            />
+
+            <PbButton
+              style="height: 10px;"
+              button-style="icon"
+              icon="fas-fa-pen fa-xs"
+              @click.native="editItem(index)"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <FormDrawer
+      v-model="state.newField"
+      :drawer-status.sync="state.drawerStatus"
+      @include-field="includeField()"
+    />
+
+    <PbConfirm
+      :title="state.confirm.title"
+      :text="state.confirm.text"
+      :icon="state.confirm.icon"
+      :confirm-button-text="state.confirm.confirmButtonText"
+      :confirm-button-color="state.confirm.confirmButtonColor"
+      :cancel-button-text="state.confirm.cancelButtonText"
+      :show="state.confirm.show"
+      @confirm="confirmAction(state.confirm.index)"
+      @cancel="cancelAction()"
     />
   </section>
 </template>
 
 <script>
-import PbForm from '../Form/Form.vue';
+import PbButton from '../../Buttons/Button/Button.vue';
+import PbConfirm from '../../NotificationsAndModals/Confirm/Confirm.vue';
+import FormDrawer from './FormDrawer.vue';
 
 export default {
   name: 'PbFormBuilder',
 
   components: {
-    PbForm,
+    PbConfirm,
+    PbButton,
+    FormDrawer,
   },
 
   props: {
@@ -24,150 +95,18 @@ export default {
   data() {
     return {
       state: {
-        entitySchema: {
-          entitySchema: {
-            type: 'object-keys',
-            label: 'Campo no formulário',
-            keyType: 'string',
-            keyLabel: 'Novo Campo',
-            contentObject: {
-              type: 'object',
-              label: 'O tal do campo novo',
-              contentObject: {
-                type: {
-                  type: 'select',
-                  label: 'Tipo do campo',
-                  required: true,
-                  enabledValue: [
-                    'html',
-                    'string',
-                    'number',
-                    'boolean',
-                    'text',
-                    'file',
-                    'datetime',
-                    'object',
-                    'array',
-                    'select',
-                    'object-keys',
-                    'custom-field',
-                  ],
-                  labelValue: [
-                    'Texto html',
-                    'Texto normal',
-                    'Numero',
-                    'verdadeiro/falso (boolean)',
-                    'Texto grande (4 linhas)',
-                    'Arquivo',
-                    'Data e hora',
-                    'Entidade',
-                    'Lista',
-                    'Seleção',
-                    'Entidade chaveada',
-                    'Campo customizado',
-                  ],
-                },
-                label: {
-                  type: 'string',
-                  label: 'Nome do campo',
-                },
-                required: {
-                  type: 'boolean',
-                  label: 'Campo obrigatório?',
-                },
-                position: {
-                  type: 'number',
-                  label: 'Posição do campo',
-                },
-                tip: {
-                  type: 'string',
-                  label: 'Dica do campo',
-                },
-                dynamic: {
-                  type: 'boolean',
-                  label: 'Campo é controlado via sistema?',
-                },
-                columnVisible: {
-                  type: 'boolean',
-                  label: 'Campo é exibido nas consultas?',
-                },
-                occult: {
-                  type: 'boolean',
-                  label: 'Campo não sera exibido?',
-                },
-                placeholder: {
-                  type: 'string',
-                  label: 'Dica no campo',
-                },
-                hasThreeState: {
-                  type: 'boolean',
-                  label: '[verdadeiro/falso (boolean)] possui tres estados',
-                },
-                filterFileTypes: {
-                  type: 'array',
-                  label: '[Arquivo] tipos de arquivo',
-                  contentArray: {
-                    type: 'string',
-                    label: 'tipo do arquivo',
-                    tip: '.txt .pdf, usar o .(ponto)',
-                  },
-                },
-                onlyDate: {
-                  type: 'boolean',
-                  label: '[Data e hora] exibir somente data',
-                },
-                contentObject: {
-                  type: 'object',
-                  label: '[Entidade] dados da entidade',
-                  contentObject: {},
-                  tip: 'Utilizar gerador para preencher este campo',
-                },
-                contentArray: {
-                  type: 'object',
-                  label: '[Lista] dados da entidade',
-                  contentObject: {},
-                  tip: 'Utilizar gerador para preencher este campo',
-                },
-                enabledValue: {
-                  type: 'array',
-                  label: '[Seleção] dados da seleção',
-                  contentArray: {
-                    type: 'string',
-                    label: 'Valor do select',
-                  },
-                },
-                labelValue: {
-                  type: 'array',
-                  label: '[Seleção] displays da seleção',
-                  contentArray: {
-                    type: 'string',
-                    label: 'Valor do select',
-                  },
-                },
-                keyType: {
-                  type: 'select',
-                  label: '[Entidade chaveada] tipo da chave',
-                  enabledValue: [
-                    'string',
-                    'number',
-                  ],
-                  labelValue: [
-                    'Texto',
-                    'Numero',
-                  ],
-                },
-                keyLabel: {
-                  type: 'string',
-                  label: '[Entidade chaveada] Nome da chave do objeto',
-                },
-                slotName: {
-                  type: 'string',
-                  label: '[Campo customizado] Nome do objeto do slot',
-                },
-              },
-            },
-          },
+        confirm: {
+          show: false,
+          index: null,
+          title: 'Você tem certeza que deseja excluir?',
+          text: 'Os campo será removido para sempre!',
+          icon: 'fas fa-exclamation-circle',
+          confirmButtonText: 'Confirmar',
+          confirmButtonColor: 'danger',
+          cancelButtonText: 'Cancelar',
         },
+        drawerStatus: false,
+        newField: {},
       },
     };
   },
@@ -182,11 +121,98 @@ export default {
       },
     },
   },
+
+  methods: {
+    includeField() {
+      this.formResponse = {
+        ...this.formResponse,
+        [this.state.newField.label]: { ...this.state.newField },
+      };
+
+      this.state.newField = {};
+      this.closeDrawer();
+    },
+
+    openDrawer() {
+      this.state.drawerStatus = true;
+    },
+
+    closeDrawer() {
+      this.state.drawerStatus = false;
+    },
+
+    newItem() {
+      this.state.newField = {};
+      this.openDrawer();
+    },
+
+    editItem(index) {
+      this.state.newField = { ...this.formResponse[index] };
+      this.openDrawer();
+    },
+
+    removeItem(index) {
+      this.$delete(this.formResponse, index);
+    },
+
+    confirmAction(index) {
+      this.removeItem(index);
+      this.state.confirm.show = false;
+    },
+
+    cancelAction() {
+      this.state.confirm.show = false;
+    },
+
+    openConfirmModal(index) {
+      this.state.confirm.index = index;
+      this.state.confirm.show = true;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .form-builder-container {
-  width: 95%;
+  padding: 16px;
+
+  .form-builder-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 16px;
+
+    .form-builder-title {
+    }
+
+    .form-builder-new-field {
+    }
+  }
+
+  .form-builder-created-fields {
+    display: flex;
+    flex-direction: column;
+
+    .form-builder-created-field {
+      display: flex;
+      justify-content: space-between;
+      margin: 16px 0 16px 0;
+
+      background: var(--color-gray);
+      box-shadow: 0px 4px 7px -1px rgba(82, 89, 91, 0.14);
+      border-radius: 8px;
+      padding: 12px;
+
+      .field-container {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .field-buttons {
+        }
+      }
+    }
+  }
 }
 </style>
