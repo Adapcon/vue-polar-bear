759378c6ce2356330a493c9f5226e5aaac8ed643
Alan Neves <38226523+alanneves@users.noreply.github.com>
2022-05-11 15:48:14 -0300

    feat: sheets component (#144)
    * feat: sheets component
    
    * feat: improve docs

--- src/components/MenusAndToolbars/Sheets/Sheets.vue
+++ src/components/MenusAndToolbars/Sheets/Sheets.vue
@@ -1,35 +1,40 @@
 <template>
   <section class="sheets-container">
     <PbButton
-      v-if="enabledToggleButton"
-      color="primary"
-      button-style="regular"
-      icon="fas fa-ellipsis-v"
+      :color="buttonColor"
+      :button-style="buttonStyle"
+      :icon="buttonIcon"
       @click.native="toggleSheets"
     />
-    <div
-      v-if="state.showSheets"
-      class="sheets layer-always-on-top fullscreen"
-      @click="toggleSheets"
-    >
+
+    <div class="sheets">
       <div
-        class="grid-buttons-container-mobile"
-        @click.stop
+        class="glass layer-overlay"
+        :class="{ open: state.showSheets }"
+        @click="toggleSheets"
+      />
+      <div
+        class="sheets-body layer-always-on-top"
+        :class="{ open: state.showSheets }"
       >
         <div
-          v-for="(item, label) in sheets"
-          :key="label"
-          style="padding: 16px;"
+          v-for="sheet in sheets"
+          :key="sheet.value"
+          class="sheets-item"
+          :class="{ active: sheet.value === state.selectedSheet }"
+          @click="selectOption(sheet.value)"
         >
-          <PbButton
-            color="gray-90"
-            button-style="regular"
-            :disabled="item.disable"
-            :icon="item.icon"
-            @click.native="selectOption(label)"
+          <PbIcon
+            v-if="sheet.icon"
+            class="icon"
+            :icon="sheet.icon"
+          />
+          <p
+            v-if="sheet.label"
+            class="pb-strong"
           >
-            {{ item.title }}
-          </PbButton>
+            {{ sheet.label }}
+          </p>
         </div>
       </div>
     </div>
@@ -38,22 +43,27 @@
 
 <script>
 import PbButton from '@pb/Buttons/Button/Button.vue';
+import PbIcon from '@pb/Miscellaneous/Icon/Icon';
 
 export default {
   name: 'PbSheets',
 
   components: {
     PbButton,
+    PbIcon,
   },
 
   props: {
-    sheets: { type: Object, default: () => ({}) },
-    enabledToggleButton: { type: Boolean, default: false },
+    sheets: { type: Array, default: () => ([]), required: true },
+    buttonIcon: { type: String, default: 'fas fa-ellipsis-v' },
+    buttonColor: { type: String, default: 'primary' },
+    buttonStyle: { type: String, default: 'regular' },
   },
 
   data() {
     return {
       state: {
+        selectedSheet: null,
         showSheets: false,
       },
     };
@@ -64,8 +74,11 @@ export default {
       this.state.showSheets = !this.state.showSheets;
     },
 
-    selectOption(label) {
-      this.$emit('option-selected', label);
+    selectOption(value) {
+      this.state.selectedSheet = value;
+      this.$emit('selectSheet', value);
+
+      this.toggleSheets();
     },
   },
 };
@@ -74,17 +87,58 @@ export default {
 <style lang="scss" scoped>
 .sheets-container {
   .sheets {
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    align-items: flex-end;
-    position: fixed;
+    .glass {
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background: var(--color-overlay);
+      opacity: 0;
+      pointer-events: none;
+      transition: opacity 0.3s ease;
 
-    .grid-buttons-container-mobile {
-      background-color: var(--color-white);
-      padding: 10px;
+      &.open {
+        pointer-events: initial;
+        opacity: 1;
+      }
+    }
+
+    .sheets-body {
+      color: var(--color-gray-90) !important;
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      opacity: 0;
+      transform: translateY(100%);
+      transition: opacity 0.3s ease, transform 0.3s ease;
+      background: var(--color-white);
       display: flex;
-      width: 100vw;
       flex-direction: column;
+      overflow: hidden;
+
+      &.open {
+        opacity: 1;
+        transform: translateY(0);
+      }
+
+      padding: 8px 0;
+
+      .sheets-item {
+        height: 51px;
+        padding-left: 18px;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+
+        .icon {
+          margin-right: 10px;
+        }
+      }
+      .active {
+        background-color: var(--color-gray) !important;
+      }
     }
   }
 }
