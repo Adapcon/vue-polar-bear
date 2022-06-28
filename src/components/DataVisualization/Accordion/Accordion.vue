ddc6389c182291d26e70d870a4622f2f9b95415c
Victor Stassun <victorstassun@gmail.com>
2022-06-28 14:16:02 -0300

    feat: add new PbAccordion component

---
+++ src/components/DataVisualization/Accordion/Accordion.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <div class="accordion">
+      <h6 class="pb">{{ title }}</h6>
+      <PbCollapseIcon
+        :is-icon-up="state.collapsed"
+        color="gray-20"
+        @click.native="toggleCollapse"
+      />
+    </div>
+    <slot v-if="state.collapsed" />
+  </section>
+</template>
+
+<script>
+import { PbCollapseIcon } from '@pb';
+
+export default {
+  name: 'PbAccordion',
+
+  components: {
+    PbCollapseIcon,
+  },
+
+  props: {
+    title: {
+      type: String,
+      default: 'Nome do campo',
+    },
+  },
+
+  data() {
+    return {
+      state: {
+        collapsed: false,
+      },
+    };
+  },
+
+  methods: {
+    toggleCollapse() {
+      this.state.collapsed = !this.state.collapsed;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.accordion {
+  width: 100%;
+  height: auto;
+  background: var(--color-gray);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 14px;
+  border-radius: 8px;
+}
+</style>
