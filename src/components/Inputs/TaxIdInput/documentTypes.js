9f91dcd059ad596fac034de8cfd4ea21c3ff23a6
Carolina Trindade <32417804+caroltrindade@users.noreply.github.com>
2023-02-21 14:04:11 +0100

    fix: corrected field formatting (#286)

--- src/components/Inputs/TaxIdInput/documentTypes.js
+++ src/components/Inputs/TaxIdInput/documentTypes.js
@@ -3,10 +3,25 @@ export const documentTypesData = {
     placeholder: '___.___.___-__',
     length: 11,
     formattedLength: 14,
+    format(value) {
+      let valueFormatted = value.replace(/\D/g, '');
+      valueFormatted = valueFormatted.replace(/(\d{3})(\d)/, '$1.$2');
+      valueFormatted = valueFormatted.replace(/(\d{3})(\d)/, '$1.$2');
+      valueFormatted = valueFormatted.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+      return valueFormatted;
+    }
   },
   cnpj: {
     placeholder: '__.___.___/____-__',
     length: 14,
     formattedLength: 18,
+    format(value) {
+      let valueFormatted = value.replace(/\D/g, '');
+      valueFormatted = valueFormatted.replace(/^(\d{2})(\d)/, '$1.$2');
+      valueFormatted = valueFormatted.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+      valueFormatted = valueFormatted.replace(/\.(\d{3})(\d)/, '.$1/$2');
+      valueFormatted = valueFormatted.replace(/(\d{4})(\d)/, '$1-$2');
+      return valueFormatted;
+    },
   },
 };
