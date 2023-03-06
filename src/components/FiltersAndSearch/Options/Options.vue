7e904d3fa51ee1992f7bc2977f05791de1ff6397
Victor Stassun <82161592+victorstassun@users.noreply.github.com>
2023-03-06 14:16:48 -0300

    Revert "[SIM-662] Feat: refatoring pb options component (#290)"
    This reverts commit a8ea937a42d4da81226988354eda882dccbab99a.

--- src/components/FiltersAndSearch/Options/Options.vue
+++ src/components/FiltersAndSearch/Options/Options.vue
@@ -1,43 +1,45 @@
 <template>
   <section class="pb-options-container">
-    <button
-      v-for="option in options"
-      :key="`${option}`"
-      :class="`option ${border}`"
-      :style="optionButtonStyle(option)"
-      @click="updateValue(option)"
-    >
-      <p
-        class="pb-strong option-text"
-        :style="optionTextColor(option)"
+    <div v-for="(option) in options" :key="`${option}`">
+      <PbButton
+        style="font-size: 16px !important; margin: 10px"
+        :color="buttonColor(option)"
+        :button-style="buttonDinamicStyle(option)"
+        @click.native="updateValue(option)"
       >
-        {{ option }}
-      </p>
-    </button>
+        <p class="pb">
+          <b>{{ option.toUpperCase() }}</b>
+        </p>
+      </PbButton>
+    </div>
   </section>
 </template>
 
 <script>
+import PbButton from '@pb/Buttons/Button/Button.vue';
 import { validateColor } from '@pb/utils/validator';
 
 export default {
   name: 'PbOptions',
+  components: {
+    PbButton,
+  },
   props: {
     value: { type: String, required: true },
     options: { type: Array, required: true },
-    border: {
+    buttonStyle: {
       type: String,
-      default: 'curved',
+      default: 'regular',
+      validator: style => [
+        'regular',
+        'outline',
+        'background',
+      ].includes(style),
     },
     color: {
       type: String,
       default: 'primary',
       validator: validateColor,
-    },
-  },
-  computed: {
-    optionWidthPercentage() {
-      return `${100 / this.options.length}%`;
     },
   },
   methods: {
@@ -47,11 +49,16 @@ export default {
     isSelected(value) {
       return this.value === value;
     },
-    optionTextColor(option) {
-      return this.isSelected(option) ? `color: var(--color-${this.color})` : 'color: var(--color-gray-20)';
+    buttonColor(option) {
+      return this.isSelected(option)
+        ? this.color
+        : 'gray-20';
     },
-    optionButtonStyle(option) {
-      return this.isSelected(option) ? `background-color: rgba(var(--color-${this.color}-rgb), 0.1);` : '';
+    buttonDinamicStyle(option) {
+      if (this.buttonStyle === 'outline')
+        return this.isSelected(option) ? 'background' : 'outline';
+
+      return this.buttonStyle;
     },
   },
 };
@@ -60,47 +67,5 @@ export default {
 <style lang="scss" scoped>
 .pb-options-container {
   display: flex;
-
-  .flat {
-    &:first-child {
-      border-radius: 12px 0 0 0;
-    }
-
-    &:last-child {
-      border-radius: 0 12px 0 0;
-      border: 1px solid var(--color-gray-10);
-    }
-  }
-
-  .curved {
-    &:first-child {
-      border-radius: 25px 0 0 25px;
-    }
-
-    &:last-child {
-      border-radius: 0 25px 25px 0;
-      border: 1px solid var(--color-gray-10);
-    }
-  }
-
-  .option {
-    display: flex;
-    max-width: 180px;
-    width: auto;
-    height: 40px;
-    background-color: var(--color-transparent);
-    align-items: center;
-    flex-direction: row;
-    justify-content: space-around;
-    border: 1px solid var(--color-gray-10);
-    border-right: 0px;
-
-    .option-text {
-      overflow: hidden;
-      text-overflow: ellipsis;
-      padding: 0 12px;
-      white-space: nowrap;
-    }
-  }
 }
 </style>
