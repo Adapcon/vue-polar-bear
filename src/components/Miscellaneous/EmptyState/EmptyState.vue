3f466e0e8db1a7915985f428b4b50f485700c62a
Victor Stassun <82161592+victorstassun@users.noreply.github.com>
2023-03-20 08:35:51 -0300

    feat: creating PbEmptyState (#300)

---
+++ src/components/Miscellaneous/EmptyState/EmptyState.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="empty-state-container">
+    <img
+      src="./EmptyFolder.svg"
+      alt="Empty Folder"
+    >
+    <p class="text-info pb">{{ textInfo }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PbEmptyState',
+  props: {
+    textInfo: {
+      type: String,
+      default: 'Sem dados no momento',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.empty-state-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .text-info {
+      margin-top: 30px !important;
+      color: var(--color-gray-40)
+  }
+}
+</style>
