0dc936edd79a78495221777061bb9afb505dd0bc
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-08-22 14:02:00 -0300

    feat: emit blob so parent component can handle formatting and stuffs

--- src/components/Inputs/AudioInput/AudioInput.vue
+++ src/components/Inputs/AudioInput/AudioInput.vue
@@ -240,16 +240,9 @@ export default {
     async formatAudio() {
       const blob = new Blob(this.audio.chunks, { type: this.mimeType });
 
-      await new Promise((resolve, reject) => {
-        const reader = new FileReader(new File([blob], 'audio'));
-        reader.readAsDataURL(blob);
-        reader.onloadend = () => {
-          this.setupAudio(reader.result);
-          this.$emit('audio', reader.result);
-          resolve();
-        };
-        reader.onerror = reject;
-      });
+      this.setupAudio(URL.createObjectURL(blob));
+
+      this.$emit('audio', blob);
     },
 
     togglePlay() {
